b6f6d1bac6f29dd4df8e8195348194a4ad7f78e8
mohammadnpak <m.nozaripak@gmail.com>
2022-03-12 18:25:56 +0330

    phase 3

--- blog/urls.py
+++ blog/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('create/', views.create_post, name='create_post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,19 @@
 from django.shortcuts import render
-
+from .models import Post
+from django.contrib.auth.models import User
+from django.utils import timezone
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(created_date__lte=timezone.now()).order_by('published_date')
+    # posts = Post.objects.all()
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+def create_post(request):
+    me=User.objects.get(username='admin')
+    # for i in range(100):
+    #     Post.objects.create(author=me,title=f'sample title {i}', text=f'sample title {i**2}')
+    p=Post.objects.get(title='sssssssssss')
+    
+
+    return render(request, 'blog/create_post.html', {})
+
+    
